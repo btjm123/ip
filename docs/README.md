cd8a81cb554ee4828549ddee6ce8830983f2211b
Benn Tan <btjm123@gmail.com>
2025-08-20 14:32:56 +0800

    Complete Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Benn User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Benn.java
@@ -0,0 +1,16 @@
+public class Benn {
+    public static void main(String[] args) {
+        String logo =
+                " ____                      _  _  _ \n" +
+                "| __ )  ___ _ __  _ __   _| || || |\n" +
+                "|  _ \\ / _ \\ '_ \\| '_ \\ / _` || || |\n" +
+                "| |_) |  __/ | | | | | | (_| || || |\n" +
+                "|____/ \\___|_| |_|_| |_|\\__,_||_||_|\n";
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm BENN");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
